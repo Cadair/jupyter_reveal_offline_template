5b8bfb0f5a1617c3d1d5d75064e016d94bd4faea
Stuart Mumford <stuart@cadair.com>
2018-04-12 12:08:06 +0100

    convert reveal to a submodule

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-jupyter-nbconvert --to slides --template reveal_custom.tpl --reveal-prefix=reveal.js-3.5.0 --SlidesExporter.file_extension=.html $@
+jupyter-nbconvert --to slides --template reveal_custom.tpl --reveal-prefix=reveal.js --SlidesExporter.file_extension=.html $@
